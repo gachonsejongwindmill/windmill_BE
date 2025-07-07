c59e15e5e2f6704ef5f463c8de61d48909ec9a14
smsamlee <smsamlee007@gmail.com>
2025-07-07 23:05:28 +0900

    token 스키마 수정

--- api/schemas/refresh_token.py
+++ api/schemas/refresh_token.py
@@ -2,4 +2,8 @@ from pydantic import BaseModel
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class RefreshToken(BaseModel):
+    refresh_token: str
