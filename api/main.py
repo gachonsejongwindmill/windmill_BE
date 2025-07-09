2d6a7bf8f2651beb1d14ede7075f1228d616d429
smsamlee <smsamlee007@gmail.com>
2025-07-09 17:56:38 +0900

    requirements.txt 추가

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app.include_router(route)
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
