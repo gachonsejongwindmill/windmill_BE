8d38ef9b9cb95ebc6956600d4ff08ea7d70d9af8
smsamlee <smsamlee007@gmail.com>
2025-07-10 00:59:07 +0900

    success response dict 형식으로 수정

--- api/responses/success_response.py
+++ api/responses/success_response.py
@@ -1,31 +1,19 @@
 from typing import Any, Optional
 from fastapi import status
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-
 
 def success_response(
     message: str = "요청이 성공적으로 처리되었습니다.",
     status_code: int = status.HTTP_200_OK,
     data: Optional[Any] = None,
-) -> JSONResponse:
+) -> dict:
     """
-    통일된 성공 응답을 반환합니다.
-
-    :param message: 응답 메시지
-    :param status_code: HTTP 상태 코드
-    :param data: 응답 데이터
-    :return: JSON 형식의 응답
+    통일된 성공 응답을 반환합니다. FastAPI에서 자동으로 JSONResponse로 처리됩니다.
     """
 
-    response_body = {
+    return {
         "success": True,
         "code": status_code,
         "message": message,
         "data": data if data is not None else {},
     }
 
-    return JSONResponse(
-        status_code=status_code,
-        content=jsonable_encoder(response_body)
-    )
