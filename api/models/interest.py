f650231792f27f840aeb01060a0ae186085e08ca
smsamlee <smsamlee007@gmail.com>
2025-07-17 17:38:55 +0900

    interest model 추가

---
+++ api/models/interest.py
@@ -0,0 +1,14 @@
+from sqlalchemy import ForeignKey,Boolean
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from api.models.abstract import AbstractBaseModel
+
+class Interest(AbstractBaseModel):
+    __tablename__ = "interest"
+
+    user_id : Mapped[str] = mapped_column(ForeignKey("user.id"))
+    stock_id : Mapped[str] = mapped_column(ForeignKey("stock.id")) 
+    interested : Mapped[bool] = mapped_column(Boolean, default=False)
+
+    user = relationship("User", back_populates="interests")
+    stock = relationship("Stock", back_populates="interests")
