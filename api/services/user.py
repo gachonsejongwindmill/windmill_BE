d67d04cbdc4997a1e33c64d9bd001212edef1065
smsamlee <smsamlee007@gmail.com>
2025-07-17 19:47:31 +0900

    user interest 중복 검열 추가

--- api/services/user.py
+++ api/services/user.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 from fastapi import Depends, status, HTTPException
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from typing import Annotated, Optional
@@ -80,6 +81,20 @@ class UserService:
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="해당 종목이 존재하지 않습니다."
             )
+        
+        existing = db.query(Interest).filter(
+            and_(
+                Interest.user_id == user.id,
+                Interest.stock_id == stock_id
+            )
+        ).first()
+
+        if existing:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="이미 관심 종목으로 등록되어 있습니다."
+            )
+
 
         interest = Interest(
             user_id=user.id,
