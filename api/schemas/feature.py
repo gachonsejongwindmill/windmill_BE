6aa0e5ef6651ef40cf87ec693acce90f360fede4
smsamlee <smsamlee007@gmail.com>
2025-07-23 17:30:35 +0900

    feature 스키마 수정

--- api/schemas/feature.py
+++ api/schemas/feature.py
@@ -1,13 +1,18 @@
 from pydantic import BaseModel, ConfigDict
 
 class FeatureBase(BaseModel):
+    ticker: str
     start: bool
-    end: bool
     high: bool
     low: bool
     volume: bool
     fixed_rate: bool
+    predict_range : int
 
-class FeatureOut(FeatureBase):
-    ticker: str
+class Featurein(FeatureBase):
     model_config = ConfigDict(from_attributes=True)
+
+class FeatureOut(BaseModel):
+    string_value: str
+    int_value1: int
+    int_value2: int
