72eb261439a0c8d7c4f122846a425ce160e3de76
smsamlee <smsamlee007@gmail.com>
2025-07-25 01:09:24 +0900

    예측값 출력 api 추가

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.routers.auth import auth
 from api.routers.user import user
 from api.routers.stock import stock
+from api.routers.predict import predict
 
 route = APIRouter()
 
