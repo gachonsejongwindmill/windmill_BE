43c240a31d70c2b67ce4f25a2681f994bab6e88e
smsamlee <smsamlee007@gmail.com>
2025-08-03 17:16:16 +0900

    데이터베이스 수정

--- api/models/avatar.py
+++ api/models/avatar.py
@@ -5,7 +5,7 @@ from api.models.abstract import AbstractBaseModel
 class Avatar(AbstractBaseModel):
     __tablename__="avatar"
 
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False, default="아바타")
     loss: Mapped[int] = mapped_column(nullable=False)
     age: Mapped[int] = mapped_column(nullable=False)
     user_id: Mapped[str] = mapped_column(ForeignKey("user.id"))
