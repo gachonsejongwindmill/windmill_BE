51e8fa432007ecaf3580a5cb982d20a18dd3c18c
smsamlee <smsamlee007@gmail.com>
2025-08-11 05:26:12 +0900

    table 수정

--- api/models/stock.py
+++ api/models/stock.py
@@ -7,8 +7,8 @@ class Stock(AbstractBaseModel):
     __tablename__ = "stock"
     name: Mapped[str] = mapped_column(nullable=False)
     ticker: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
-    price: Mapped[float | None] = mapped_column(Numeric(12, 2), nullable=True)
-    change_rate: Mapped[float | None] = mapped_column(Numeric(5, 2), nullable=True)
+    price: Mapped[float] = mapped_column(Numeric(12, 2), nullable=True)
+    change_rate: Mapped[float] = mapped_column(Numeric(5, 2), nullable=True)
 
     interests = relationship("Interest", back_populates="stock", cascade="all, delete-orphan")
     mystocks = relationship("MyStock", back_populates="stock", cascade="all, delete-orphan")
